2f9d6d5f292a4a11079ea95747833ce03f956d0c
themartinsjoseph <themartinsjoseph@gmail.com>
2017-04-05 08:56:22 -0700

    established routes and controller

---
+++ nationalparks_app/app/controllers/parks_controller.rb
@@ -0,0 +1,35 @@
+class ParksController < ApplicationController
+
+  def index
+  	 @parks = Park.all
+  end
+
+  def new
+  	@park = Park.new
+  end
+
+  def create
+  	Park.create(park_params)
+  	redirect_to parks_path
+  end
+
+  def edit
+  	@park = Park.find(params[:id])
+  end
+
+  def show
+  	@park = Park.find(params[:id])
+  end
+
+  def update
+  	park = Park.find(params[:id])
+  	park.update(park_params)
+  	redirect_to parks_path
+  end
+
+  def destroy
+  	Park.find(params[:id]).delete
+  	redirect_to '/'
+  end
+
+end
--- nationalparks_app/config/routes.rb
+++ nationalparks_app/config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
-  root 'main#index'
+  
+  root 'parks#index'
 
-  get 'park' => 'main#park'
+  get 'parks/index'
 
-  resources :photos
+  get 'parks/new'
+
+  get 'parks/edit'
+
+  get 'parks/:id' => 'parks#show'
+
+  resources :parks
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+
+
+
+
